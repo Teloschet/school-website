5d9fe674b8b6a80938c7a446e68d7a9b8cea332d
Teloschet <frapgameroficial@hotmail.com>
2020-12-23 16:51:17 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Telos, Teloschet">
+    <meta name="description" content="Website for schools!">
+    <meta name="keywords" content="School, Telos, Teloschet, Study">
+
+    <title>School Project</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <script src="assets/js/main.js" type="text/javascript"></script>
+</head>
+<body>
+    <header>
+        <div class="container">
+        <button class="btn">Matrículas 2021</button>
+            <div class="navbar">
+                <div class="logo">
+                    <h1>School Name</h1>
+                </div>
+                </div>
+        </div>
+        <div id="tudo">
+                <nav>
+                    <ul>
+                        <li><a href="">O Colégio</a></li>
+                        <li><a href="">Ensino</a></li>
+                        <li><a href="">Base de Conhecimento</a></li>
+                        <li><a href="">Área do Aluno</a></li>
+                        <li><a href="">Contato</a></li>
+                    </ul>
+                </nav>
+</div>
+    </header>
+
+    <div id="acesso">
+        <h1>Bem-Vindo à SCHOOL NAME</h1>
+        <p><a href="">Saiba mais</a> sobre a SCHOOL NAME</p>
+
+        <form action="">
+            <label>NOME DE USUÁRIO</label><br>
+            <input type="text">
+            <br><br>
+            <label>SENHA</label><br>
+            <input type="password">
+            <br><br>
+            <input type="submit" value="ACESSAR">
+
+            <br>
+            <p>Não possui cadastro? <a href="">Obtenha seu acesso</a></p>
+            <br><br>
+            <p>Precisa de Suporte? Entre em contato pelo e-mail</p>
+            <br>
+            <p><a href="">suporte@schoolname.com.br</a> ou ligue para 0800 77 00012</p>
+        </form>
+    </div>
+
+    <div id="topbg">
+        <h1>Aprender. Ensinar. Crescer.</h1>
+        <p>Excelência em desenvolvimento acadêmico</p>
+
+        <button class="bn1">Explore</button>
+        <button class="bn2">Contato</button>
+    </div>
+
+    <section class="grid">
+        <div>
+            <img src="assets/img/naom_5a5f65d5dcffa.jpg">
+            <h1>Gerando a Educação</h1>
+            <p>Lorem ipsum fermentum ad suscipit purus sodales nec auctor, sociosqu iaculis risus vulputate mi non. </p>
+        </div>
+
+        <div>
+            <img src="assets/img/dois-feliz-estudantes-com-mochilas-e-livros-em-seu-maos-sorrindo-camera_8353-6384.webp">
+            <h1>Nova geração</h1>
+            <p>Lorem ipsum fermentum ad suscipit purus sodales nec auctor, sociosqu iaculis risus vulputate mi non. </p>
+        </div>
+
+        <div>
+            <img src="assets/img/estudante-de-sorriso_1301-114.webp">
+            <h1>Estágios em Inovação</h1>
+            <p>Lorem ipsum fermentum ad suscipit purus sodales nec auctor, sociosqu iaculis risus vulputate mi non. </p>
+        </div>
+
+        <div class="anuncio">
+            <img src="assets/img/unnamed.jpg">
+        </div>
+    </section>
+
+    <section class="grid1">
+        <div>
+            <video src="assets/img/video.mp4" type="video/mp4" autoplay="" muted="" loop="" width="700"></video>
+            <h1>Junte-se ao programa estudantil</h1><br>
+            <p>Lorem ipsum massa nunc id egestas fusce orci integer, sed dui inceptos laoreet ipsum facilisis condimentum nostra, a pellentesque dictumst nam leo aliquet elit. pharetra ornare pellentesque mollis felis fermentum porta augue curae litora, curabitur facilisis tempor sodales arcu conubia lectus lacinia, imperdiet erat lectus justo quisque accumsan leo quisque.</p>    
+        </div>
+    </section>
+
+    <footer>
+        <div class="footer-content">
+            <h3>School Name</h3>
+            <p>Lorem ipsum fermentum ad suscipit purus sodales nec auctor, sociosqu iaculis risus vulputate mi non.</p>
+            <ul class="socials">
+                <li><a href="">Facebook</a></li>
+                <li><a href="">Twitter</a></li>
+                <li><a href="">Youtube</a></li>
+                <li><a href="">Linkedin</a></li>
+                <li><a href="">Aplicativo</a></li>
+            </ul>
+        </div>
+        <div class="footer-bottom">
+            <p>copyright &copy; 2020 - 2021. designed by <span>Telos</span></p>
+        </div>
+    </footer>
+</body>
+</html>
